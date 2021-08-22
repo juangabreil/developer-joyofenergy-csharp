7d50e2f21de3f7be8ba764347d5020f65f80adb9
Juanga JC <juangabreil@gmail.com>
2021-08-22 22:22:14 +0200

    Extract cost calculator

---
+++ JOIEnergy/Services/CostCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JOIEnergy.Domain;
+
+namespace JOIEnergy.Services
+{
+    public static class CostCalculator
+    {
+        public static decimal CalculateCost(IList<ElectricityReading> electricityReadings, PricePlan pricePlan)
+        {
+            var average = CalculateAverageReading(electricityReadings);
+            var timeElapsed = CalculateTimeElapsed(electricityReadings);
+            var averagedCost = average / timeElapsed;
+            return averagedCost * pricePlan.UnitRate;
+        }
+
+        private static decimal CalculateAverageReading(IList<ElectricityReading> electricityReadings)
+        {
+            var newSummedReadings = electricityReadings.Select(readings => readings.Reading)
+                .Aggregate((reading, accumulator) => reading + accumulator);
+
+            return newSummedReadings / electricityReadings.Count();
+        }
+
+        private static decimal CalculateTimeElapsed(IList<ElectricityReading> electricityReadings)
+        {
+            var first = electricityReadings.Min(reading => reading.Time);
+            var last = electricityReadings.Max(reading => reading.Time);
+
+            return (decimal) (last - first).TotalHours;
+        }
+    }
+}
--- JOIEnergy/Services/PricePlanService.cs
+++ JOIEnergy/Services/PricePlanService.cs
@@ -16,28 +16,6 @@ namespace JOIEnergy.Services
             _meterReadingService = meterReadingService;
         }
 
-        private static decimal CalculateAverageReading(IList<ElectricityReading> electricityReadings)
-        {
-            var newSummedReadings = electricityReadings.Select(readings => readings.Reading).Aggregate((reading, accumulator) => reading + accumulator);
-
-            return newSummedReadings / electricityReadings.Count();
-        }
-
-        private static decimal CalculateTimeElapsed(IList<ElectricityReading> electricityReadings)
-        {
-            var first = electricityReadings.Min(reading => reading.Time);
-            var last = electricityReadings.Max(reading => reading.Time);
-
-            return (decimal)(last - first).TotalHours;
-        }
-        private static decimal CalculateCost(IList<ElectricityReading> electricityReadings, PricePlan pricePlan)
-        {
-            var average = CalculateAverageReading(electricityReadings);
-            var timeElapsed = CalculateTimeElapsed(electricityReadings);
-            var averagedCost = average/timeElapsed;
-            return averagedCost * pricePlan.UnitRate;
-        }
-
         public IDictionary<string, decimal> GetConsumptionCostOfElectricityReadingsForEachPricePlan(string smartMeterId)
         {
             var electricityReadings = _meterReadingService.GetReadings(smartMeterId);
@@ -47,7 +25,9 @@ namespace JOIEnergy.Services
                 return new Dictionary<string, decimal>();
             }
             
-            return _pricePlans.ToDictionary(plan => plan.EnergySupplier.ToString(), plan => CalculateCost(electricityReadings, plan));
+            return _pricePlans.ToDictionary(
+                plan => plan.EnergySupplier.ToString(), 
+                plan => CostCalculator.CalculateCost(electricityReadings, plan));
         }
     }
 }
